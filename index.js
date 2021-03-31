c86e7c190ec5aab4ad3f0e484ea4e4088269ab4a
ogunniyi <ogunniyiokebukola@gmail.com>
2021-03-31 14:55:30 +0000

    Take-Home-School-Shopping

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function mostExpensiveItemName(arr=items){
 }
 
 // Prompt 3 : priceLookup
-function priceLookup(arr,name){
+function priceLookup(arr=items,name){
   item = arr[arr.findIndex(obj=>obj.itemName===name)]
   if(item) return item.price
   return "No item found with that name"
